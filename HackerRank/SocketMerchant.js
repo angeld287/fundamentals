7f65239c1887199337f103575eb63616a0610de1
Angel Angeles <daniel_1234@hotmail.es>
2022-08-02 16:40:58 -0400

    adding plusminus and timeconversion

--- HackerRank/SocketMerchant.js
+++ HackerRank/SocketMerchant.js
@@ -6,6 +6,27 @@
     Hay una gran pila de calcetines que deben emparejarse por color.
     Dada una matriz de números enteros que representan el color de cada calcetín,
     determinar cuántos pares de calcetines del mismo color hay.
+
+
+    Example
+
+    There is one pair of color  and one of color. 
+    There are three odd socks left, one of each color. The number of pairs is .
+
+    Function Description
+
+    Complete the sockMerchant function in the editor below.
+    sockMerchant has the following parameter(s):
+
+    int n: the number of socks in the pile
+    int ar[n]: the colors of each sock
+    Returns
+
+    int: the number of pairs
+    Input Format
+
+    The first line contains an integer , the number of socks represented in .
+    The second line contains  space-separated integers, , the colors of the socks in the pile.
     
     https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=internal-search
 */
