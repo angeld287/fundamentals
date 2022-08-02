e510cb27229719663122f88d926d4273ee038115
Angel Angeles <daniel_1234@hotmail.es>
2022-08-02 17:12:25 -0400

    ading lonelyinteger

---
+++ HackerRank/LonelyInteger.js
@@ -0,0 +1,8 @@
+/**
+ * Given an array of integers, where all elements but one occur twice, find the unique element.
+ * 
+ * Function Description
+ *  Complete the lonelyinteger function in the editor below.
+ *  lonelyinteger has the following parameter(s): 
+ *  int a[n]: an array of integers
+ */
