81a5acc800874a5d96d602541055d4583d4a612b
Angel Angeles <daniel_1234@hotmail.es>
2022-08-05 21:32:36 -0400

    creating prime number alghoritm

---
+++ code/getPrimeNumbers.js
@@ -0,0 +1,14 @@
+let number = 63;
+
+let primeNumbers = []
+
+for (let i = 0; i < number; i++) {
+    if (number % i !== 0) {
+        continue
+    }
+    console.log(number, i, number % i)
+    primeNumbers.push(number)
+}
+
+
+console.log(primeNumbers);
