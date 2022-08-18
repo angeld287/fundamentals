b1840db8c26595e98d401f5b4e2a6f5cb92f990c
Angel Angeles <daniel_1234@hotmail.es>
2022-08-18 18:44:35 -0400

    adding graph algoritms spesifications

---
+++ DataStructure/Graphs/componentsInGraph.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ *      There are (2 X N) nodes in an undirected graph, and a 
+ *      number of edges connecting some nodes. In each edge, 
+ *      the first value will be between 1 and N, inclusive. 
+ *      The second node will be between N + 1 and 2 X N, inclusive. 
+ *      Given a list of edges, determine the size of the 
+ *      smallest and largest connected components that have 2
+ *      or more nodes. A node can have any number of connections. 
+ *      The highest node value will always be connected to at least 1
+ *      other node.
+ * 
+ *      Hay (2 X N) nodos en un gráfico no dirigido, y un
+ *      número de aristas que conectan algunos nodos. En cada borde,
+ *      el primer valor estará entre 1 y N, ambos inclusive.
+ *      El segundo nodo estará entre N + 1 y 2 X N, ambos inclusive.
+ *      Dada una lista de aristas, determine el tamaño de la
+ *      componentes conectados más pequeños y más grandes que tienen 2
+ *      o más nodos. Un nodo puede tener cualquier número de conexiones.
+ *      El valor de nodo más alto siempre estará conectado a al menos 1
+ *      otro nodo.
+ * 
+ */
