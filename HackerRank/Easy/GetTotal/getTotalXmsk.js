5464d41fa8b8a003984bf60af47a981160fa254c
Angel Angeles <daniel_1234@hotmail.es>
2022-08-22 18:24:55 -0400

    addin getTotalx Soluciton found

---
+++ HackerRank/Easy/GetTotal/getTotalXmsk.js
@@ -0,0 +1,32 @@
+
+function getTotalX(a, b) {
+    let output = []
+    // track int
+    for (let i = 1; i <= Math.max(...b); i++) {
+        let mods = 0
+        // track elems of a
+        for (let j = 0; j < a.length; j++) {
+            mods += i % a[j]
+        }
+        if (mods === 0) {
+            // track elems of b
+            for (let k = 0; k < b.length; k++) {
+                mods += b[k] % i
+            }
+            if (mods === 0) {
+                output.push(i)
+            } else continue
+        } else continue
+    }
+
+    const result = [
+
+    ];
+
+    console.log(Math.max(...b))
+    return output.length
+
+}
+
+console.log(getTotalX([3, 4], [24, 48]))
+console.log(getTotalX([2, 4], [16, 32, 96]))
