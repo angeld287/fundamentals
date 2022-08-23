eb5d0055544b06a6078057390e7f2af452fab09d
Angel Angeles <daniel_1234@hotmail.es>
2022-08-23 07:57:23 -0400

    fixing bug

--- HackerRank/Easy/breakingRecords.js
+++ HackerRank/Easy/breakingRecords.js
@@ -2,8 +2,8 @@ function breakingRecords(scores) {
     let highest = { points: scores[0], times: 0 };
     let lowest = { points: scores[0], times: 0 };
     scores.forEach(e => {
-        highest = (highest.points < e) ? { points: e.points, times: highest.times + 1 } : highest;
-        lowest = (lowest.points > e) ? { points: e.points, times: lowest.times + 1 } : lowest;
+        highest = (highest.points < e) ? { points: e, times: highest.times + 1 } : highest;
+        lowest = (lowest.points > e) ? { points: e, times: lowest.times + 1 } : lowest;
     });
 
     return [highest.times, lowest.times]
