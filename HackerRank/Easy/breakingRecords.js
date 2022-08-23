6d43e29166569d90490811d901aeed570ff4b0cc
Angel Angeles <daniel_1234@hotmail.es>
2022-08-23 07:54:24 -0400

    creating breakingRecords structore

--- HackerRank/Easy/breakingRecords.js
+++ HackerRank/Easy/breakingRecords.js
@@ -1,9 +1,12 @@
 function breakingRecords(scores) {
-    let highest = {};
-    let lowest = {};
-    scores.forEach(element => {
-
+    let highest = { points: scores[0], times: 0 };
+    let lowest = { points: scores[0], times: 0 };
+    scores.forEach(e => {
+        highest = (highest.points < e) ? { points: e.points, times: highest.times + 1 } : highest;
+        lowest = (lowest.points > e) ? { points: e.points, times: lowest.times + 1 } : lowest;
     });
+
+    return [highest.times, lowest.times]
 }
 
 console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]))
