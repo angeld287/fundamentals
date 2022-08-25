856d671d41b881311423eee49041b5ec41292612
Angel Angeles <daniel_1234@hotmail.es>
2022-08-25 07:37:03 -0400

    first dev in divisibleSumPairs

--- HackerRank/Easy/divisibleSumPairs.js
+++ HackerRank/Easy/divisibleSumPairs.js
@@ -1,5 +1,13 @@
 function divisibleSumPairs(n, k, ar) {
-    return n
+    const result = []
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; (j < n && i > j); j++) {
+            const sum = ar[i] + ar[j];
+            if (sum % k)
+                result.push(sum)
+        }
+    }
+    return result.length
 }
 
 console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));
